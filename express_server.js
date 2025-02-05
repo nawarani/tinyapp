4a3fbaf2ca6aabc091bcb78540dfd6d0e37d0cd3
Anika Nawar <nawarani47@gmail.com>
2025-02-05 12:09:24 -0500

    added register route

--- express_server.js
+++ express_server.js
@@ -11,6 +11,20 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com",
 };
 
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
@@ -32,14 +46,14 @@ app.get("/hello", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { 
     urls: urlDatabase,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
   const templateVars = { 
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_new", templateVars);
 });
@@ -48,7 +62,7 @@ app.get("/urls/:id", (req, res) => {
   const templateVars = { 
     id: req.params.id, 
     longURL: urlDatabase[req.params.id],
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("urls_show", templateVars);
 });
@@ -56,7 +70,7 @@ app.get("/urls/:id", (req, res) => {
 app.get("/register", (req, res) => {
   const templateVars = { 
     id: req.params.id,
-    username: req.cookies["username"]
+    user: users[req.cookies["user_id"]]
   };
   res.render("register.ejs", templateVars);
 });
@@ -98,13 +112,31 @@ app.post("/urls/:id/update", (req, res) => {
   res.redirect(`/urls`);
 });
 
-app.post("/login", (req, res) => {
-  res.cookie("username", req.body.username);
+// app.post("/login", (req, res) => {
+//   res.cookie("username", req.body.username);
+//   res.redirect(`/urls`);
+// });
+
+app.post("/logout", (req, res) => {
+  res.clearCookie('user_id');
   res.redirect(`/urls`);
 });
 
-app.post("/logout", (req, res) => {
-  res.clearCookie('username');
+app.post("/register", (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let id = generateRandomString();
+  while (users[id]) {
+    id = generateRandomString();
+  }
+  let user = {
+    id: id,
+    email: email,
+    password: password
+  };
+  users[id] = user;
+  res.cookie("user_id", id);
   res.redirect(`/urls`);
 });
+
 
